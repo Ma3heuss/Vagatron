5f462516ff705907977297f9c3695777a4e51a15
Matheus Moreira <ma3heus2205@gmail.com>
2026-06-10 18:10:15 -0300

    UsuarioF

--- Vagatron-main/js/usuarios.js
+++ Vagatron-main/js/usuarios.js
@@ -2,17 +2,34 @@ let usuarios = [];
 
 function adicionarUsuario(){
 
+    let matricula = document.getElementById("matricula").value;
     let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
     let cargo = document.getElementById("cargo").value;
 
+    if(
+        matricula == "" ||
+        nome == "" ||
+        email == "" ||
+        cargo == ""
+    ){
+        alert("Preencha todos os campos!");
+        return;
+    }
+
     usuarios.push({
+        matricula,
         nome,
         email,
         cargo
     });
 
     atualizarTabela();
+
+    document.getElementById("matricula").value = "";
+    document.getElementById("nome").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("cargo").value = "";
 }
 
 function atualizarTabela(){
@@ -25,11 +42,12 @@ function atualizarTabela(){
 
         tabela.innerHTML += `
         <tr>
+            <td>${usuarios[i].matricula}</td>
             <td>${usuarios[i].nome}</td>
             <td>${usuarios[i].email}</td>
             <td>${usuarios[i].cargo}</td>
             <td>
-                <button onclick="excluirUsuario(${i})">
+                <button class="excluir" onclick="excluirUsuario(${i})">
                     Excluir
                 </button>
             </td>
@@ -38,9 +56,9 @@ function atualizarTabela(){
     }
 }
 
-function excluirUsuario(i){
+function excluirUsuario(indice){
 
-    usuarios.splice(i,1);
+    usuarios.splice(indice,1);
 
     atualizarTabela();
 }
